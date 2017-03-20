586a1cf9ddb266eedbd7c648159b89b6f3aa091f
Adam Sanders <adam.sanders@emory.edu>
2017-03-20 13:33:50 -0400

    Only trigger default $page_name if $page_name hasn't been set yet

--- index.php
+++ index.php
@@ -10,12 +10,14 @@ $request_uri = str_replace('/', '', $_SERVER['REQUEST_URI']);
 if ($request_uri == '')
 	$page_name = 'landing_page.php';
 
-
 // Check if URL is a username
 if (!$page_name) {
 	$user_prep = $db->prepare("SELECT * FROM users WHERE username = ?");
 	$user_prep->execute(array($request_uri));
 	$user = $user_prep->fetchObject();
+	
+	if ($user)
+		$page_name = 'user.php';
 }
 
 // TODO: Check for event URL
@@ -23,9 +25,9 @@ if (!$page_name) {
 }
 
 // If none of the others triggered, go to landing_page
-$page_name = 'landing_page.php';
+if (!$page_name)
+	$page_name = 'landing_page.php';
 ?>
-
 <!DOCTYPE html>
 <html>
 
@@ -33,8 +35,8 @@ $page_name = 'landing_page.php';
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
-	
+	<link rel="stylesheet" type="text/css" href="http://adam-sanders.com/css/style.css" />
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
