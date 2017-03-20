c6ec1ddf8a62faba2f950096473dc7106cff5a36
Adam Sanders <adam.sanders@emory.edu>
2017-03-20 13:33:11 -0400

    User page

--- user.php
+++ user.php
@@ -1,6 +1,29 @@
 <div id="user_wrapper">
-	
-</div>
+		<div class="col-md-12">
+			<h1 class="page-header text-center"><?=$user->full ?></h1>
+		</div>
+		
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<h2>User: <?=$user->username ?></h2>
+					<h3>Liked events</h3>
+
+					<table class="table"><tbody>
+						<tr><td>
+							Planned Parenthood
+						</td></tr>
+						<tr><td>
+							Immigration Restrictions
+						</td></tr>
+					</tbody></table>
+				</div>
+			</div>
+		</div>
+
+
+
+	</div>
 
 <script>
 	
