ec66fdb6adad4edab4e18b21f6ea8c7cc05a973b
Gyeongmo (Kevin) Kim <kgm_1004@naver.com>
2017-04-05 19:41:35 -0400

    Updated navbar.php and corresponding css
    lorem ipsum

--- navbar.php
+++ navbar.php
@@ -1,23 +1,32 @@
-<nav class="navbar navbar-static-top">
-	<div class="logoimg">
-		<a href="#"><img class="logo" src="http://virtual-force.com/wp-content/uploads/2015/05/mockup-icon.svg"></a>
-		<a href="#" class="sitename"><span class = "sitename">FindMyProtest</span></a>
-		<div class="menubutton">
-			<div class="dropdown">
-				<button class="btn btn-primary dropdown-toggle red" type="button" data-toggle="dropdown">
-						<span class="glyphicon glyphicon-menu-down red"></span>
-					</button>
-				<ul class="dropdown-menu pull-right">
-					<li><a href="#">About Us</a></li>
-					<li><a href="#">Contact Us</a></li>
-					<li><a href="#">Help</a></li>
-					<li><a href="#">Settings</a></li>
-				</ul>
-			</div>
-		</div>
-		<p class="acctparagraph">
-			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#loginmodal"><span class = "accountbuttons">Log In</span></button>
-			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#signupmodal"><span class = "accountbuttons">Sign Up</span></button>
-		</p>
-	</div>
+  <nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#buttons" aria-expanded="false">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+      </button>
+      <a href="#"><img class="logo" src="img/logo.png">
+</a>
+      <a href="#" class="navbar-brand">FindMyProtest</a>
+      </div>
+      
+      <div class = "collapse navbar-collapse" id="buttons">
+        <ul class="nav navbar-nav navbar-right">
+          <li class="buttonright"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#loginmodal"><span class = "accountbuttons">Log In</span></button> </li>
+          <li class="buttonright"><button type="button" class="btn btn-default" data-toggle="modal" data-target="#signupmodal"><span class = "accountbuttons">Sign Up</span></button> </li>
+          <li class="buttonright"><div class="dropdown">
+            <button class="btn btn-primary dropdown-toggle red" type="button" data-toggle="dropdown">
+            <span class="glyphicon glyphicon-menu-down red"></span>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Settings</a></li>
+            </ul>
+          </div></li>
+        </ul>
+     </div>
+  </div>
 </nav>
