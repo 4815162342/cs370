c122e94cd26cfeb0167be22f74b3b61b89c29391
Adam Sanders <adam.sanders@emory.edu>
2017-04-05 19:50:26 -0400

    Fixes.

---
+++ search_bar.php
@@ -0,0 +1,13 @@
+<select class="form-control">
+	<option disabled selected>Select City</option>
+	<?php
+		$qry = $db->query("SELECT city FROM locations");
+		
+		while ($city = $qry->fetchColumn()) {
+			echo "<option>$city</option>";
+		}
+	?>
+</select>
+<input type="date" class="form-control" id="date-input" placeholder="MM/DD/YYYY">
+<input type="text" class="form-control" id="issue-input" placeholder="Issue or Topic">
+<button type="button" class="btn btn-default findbutton" onclick="find()"><span class="findtext">Find</span></button>
