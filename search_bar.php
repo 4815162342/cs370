eb1036ec17a683ac04ab15d388cf6ba9933119d3
Adam Sanders <adam.sanders@emory.edu>
2017-04-06 18:52:27 -0400

    Spruced up search bar

--- search_bar.php
+++ search_bar.php
@@ -8,6 +8,18 @@
 		}
 	?>
 </select>
-<input type="date" class="form-control" id="date-input" placeholder="MM/DD/YYYY">
-<input type="text" class="form-control" id="issue-input" placeholder="Issue or Topic">
+
+<input placeholder="Date" class="form-control" type="text" onfocus="(this.type='date')"  id="date-input"> 
+
+<select id="issue-input" class="form-control">
+	<option disabled selected>Issue or Topic</option>
+	<?php
+		$qry = $db->query("SELECT name FROM topics");
+		
+		while ($name = $qry->fetchColumn()) {
+			echo "<option>$name</option>";
+		}
+	?>
+</select>
+
 <button type="button" class="btn btn-default findbutton" onclick="find()"><span class="findtext">Find</span></button>
