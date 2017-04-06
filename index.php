0c183f13b8468543d408a552203bfa71b21c12ca
Jonathan Liang <j.liang8888@gmail.com>
2017-04-06 18:13:12 -0400

    Fixed some CSS issues
    -Menu glyphicon on search results page (possible bugs)
    -Navbar logo size on search results page
    -Centered logo on homepage

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if (!$page_name) {
 	$user_prep = $db->prepare("SELECT * FROM users WHERE username = ?");
 	$user_prep->execute(array($request_uri));
 	$user = $user_prep->fetchObject();
-	
+
 	if ($user)
 		$page_name = 'user.php';
 }
@@ -25,7 +25,7 @@ if (!$page_name) {
 	$event_prep = $db->prepare("SELECT * FROM events WHERE URL = ?");
 	$event_prep->execute(array($request_uri));
 	$event = $event_prep->fetchObject();
-	
+
 	if ($event)
 		$page_name = 'event.php';
 
@@ -42,7 +42,6 @@ if (!$page_name)
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans">
-	<link rel="stylesheet" type="text/css" href="/css/style.css">
 	<link rel="stylesheet" type="text/css" href="/css/common.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
