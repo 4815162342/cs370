473f828c7a9577c859d1829526c461f00e8e4193
Adam Sanders <adam.sanders@emory.edu>
2017-04-09 10:00:51 -0400

    Fixed indentation

--- navbar.php
+++ navbar.php
@@ -1,26 +1,26 @@
 <nav class="navbar navbar-static-top">
-		<div class="logoimg">
-			<a href="/"><img class="logo" src="/img/logo.png"></a>
-			<a href="/" class="sitename"><span class = "sitename">FindMyProtest</span></a>
-			<div class="menubutton">
-				<div class="dropdown">
-					<button class="btn btn-primary dropdown-toggle red" type="button" data-toggle="dropdown">
-						<span class="glyphicon glyphicon-menu-down"></span>
-					</button>
-					<ul class="dropdown-menu pull-right">
-						<li><a href="#" data-toggle="modal" data-target="#aboutusmodal">About Us</a></li>
-						<li><a href="#" data-toggle="modal" data-target="#contactusmodal">Contact Us</a></li>
-					</ul>
-				</div>
+	<div class="logoimg">
+		<a href="/"><img class="logo" src="/img/logo.png"></a>
+		<a href="/" class="sitename"><span class = "sitename">FindMyProtest</span></a>
+		<div class="menubutton">
+			<div class="dropdown">
+				<button class="btn btn-primary dropdown-toggle red" type="button" data-toggle="dropdown">
+					<span class="glyphicon glyphicon-menu-down"></span>
+				</button>
+				<ul class="dropdown-menu pull-right">
+					<li><a href="#" data-toggle="modal" data-target="#aboutusmodal">About Us</a></li>
+					<li><a href="#" data-toggle="modal" data-target="#contactusmodal">Contact Us</a></li>
+				</ul>
 			</div>
-			<p id="loggedOutButtons" class="acctparagraph">
-				<button type="button" class="btn btn-default" data-toggle="modal" data-target="#loginmodal"><span class = "accountbuttons">Log In</span></button>
-				<button type="button" class="btn btn-default" data-toggle="modal" data-target="#signupmodal"><span class = "accountbuttons">Sign Up</span></button>
-			</p>
-<!--
-			<div id="loggedInButtons">
-				<button type="button" class="btn btn-default" data-toggle="modal" data-target="#createEvent"><span class = "accountbuttons">Create Event</span></button>
-			</div>
--->
 		</div>
-	</nav>
+		<p id="loggedOutButtons" class="acctparagraph">
+			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#loginmodal"><span class = "accountbuttons">Log In</span></button>
+			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#signupmodal"><span class = "accountbuttons">Sign Up</span></button>
+		</p>
+<!--
+		<div id="loggedInButtons">
+			<button type="button" class="btn btn-default" data-toggle="modal" data-target="#createEvent"><span class = "accountbuttons">Create Event</span></button>
+		</div>
+-->
+	</div>
+</nav>
