90c7e98bd212ae47b0d69a944302750841210ad4
Jonathan Liang <j.liang8888@gmail.com>
2017-04-13 19:18:34 -0400

    Attempt to fix formatting
    search bar css

--- search_bar.php
+++ search_bar.php
@@ -1,21 +1,21 @@
-<select id="location-input" class="form-control">
+<div class="searchbar"><select id="location-input" class="form-control">
 	<option disabled selected>Select City</option>
 	<?php
 		$qry = $db->query("SELECT city FROM locations");
-		
+
 		while ($city = $qry->fetchColumn()) {
 			echo "<option>$city</option>";
 		}
 	?>
 </select>
 
-<input placeholder="Date" class="form-control" type="text" onfocus="(this.type='date')"  id="date-input"> 
+<input placeholder="Date" class="form-control" type="text" onfocus="(this.type='date')"  id="date-input">
 
 <select id="issue-input" class="form-control">
 	<option disabled selected>Issue or Topic</option>
 	<?php
 		$qry = $db->query("SELECT name FROM topics");
-		
+
 		while ($name = $qry->fetchColumn()) {
 			echo "<option>$name</option>";
 		}
@@ -23,3 +23,4 @@
 </select>
 
 <button type="button" class="btn btn-default findbutton" onclick="find()"><span class="findtext">Find</span></button>
+</div>
