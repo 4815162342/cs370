f53c38fd360bbca18163aa16034b2db86cfe6be1
Adam Sanders <adam.sanders@emory.edu>
2017-04-20 19:37:52 -0400

    spaces to tabs

--- ajax/saveEvent.php
+++ ajax/saveEvent.php
@@ -1,13 +1,13 @@
 <?php
-  include($_SERVER['DOCUMENT_ROOT']."/lib/functions.php");
-  include($_SERVER['DOCUMENT_ROOT'].'/../db.php');
+	include($_SERVER['DOCUMENT_ROOT']."/lib/functions.php");
+	include($_SERVER['DOCUMENT_ROOT'].'/../db.php');
 
-  //include_once($_SERVER['DOCUMENT_ROOT']."/lib/include.php");
-  function savedEvents($event_id){
-    global $user;
-  	$event_prep = $db->prepare("INSERT INTO events_saved (eid,uid) VALUES (?,?)");
-  	$event_prep->execute(array($event_id,$user->id));
-  }
+	//include_once($_SERVER['DOCUMENT_ROOT']."/lib/include.php");
+	function savedEvents($event_id){
+		global $user;
+		$event_prep = $db->prepare("INSERT INTO events_saved (eid,uid) VALUES (?,?)");
+		$event_prep->execute(array($event_id,$user->id));
+	}
 
-  savedEvents($_POST['eventToSave']);
+	savedEvents($_POST['eventToSave']);
 ?>
