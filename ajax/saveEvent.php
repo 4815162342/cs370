98823778ac44535eac19a71366c9bc3f85d3f551
Adam Sanders <adam.sanders@emory.edu>
2017-04-20 19:48:58 -0400

    small syntax

--- ajax/saveEvent.php
+++ ajax/saveEvent.php
@@ -2,12 +2,11 @@
 	include($_SERVER['DOCUMENT_ROOT']."/lib/functions.php");
 	include($_SERVER['DOCUMENT_ROOT'].'/../db.php');
 
-	//include_once($_SERVER['DOCUMENT_ROOT']."/lib/include.php");
-	function savedEvents($event_id){
+	function savedEvent($event_id){
 		global $user;
 		$event_prep = $db->prepare("INSERT INTO events_saved (eid,uid) VALUES (?,?)");
 		$event_prep->execute(array($event_id,$user->id));
 	}
 
-	savedEvents($_POST['eventToSave']);
+	savedEvent($_POST['eventToSave']);
 ?>
