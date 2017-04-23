ef4fd9b3f307c566d50344710a9bf5676e1b0f18
alaraaa86 <alexandralara86@gmail.com>
2017-04-23 16:53:06 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,27 @@
 <?php
 include('../db.php');
 include('lib/functions.php');
-
 // Handle URI
 $page_name = false;
 $request_uri = str_replace('/', '', $_SERVER['REQUEST_URI']);
-
 // Check if URL is landing_page
 if ($request_uri == '')
 	$page_name = 'landing_page';
-
 // Check if URL is a username
 if (!$page_name) {
 	$user_prep = $db->prepare("SELECT * FROM users WHERE username = ?");
 	$user_prep->execute(array($request_uri));
 	$user = $user_prep->fetchObject();
-
 	if ($user)
 		$page_name = 'user';
 }
-
 // Check for event URL
 if (!$page_name) {
 	$event_prep = $db->prepare("SELECT * FROM events WHERE URL = ?");
 	$event_prep->execute(array($request_uri));
 	$event = $event_prep->fetchObject();
-
 	if ($event)
 		$page_name = 'event';
-
 }
 ?>
 <!DOCTYPE html>
@@ -37,6 +30,8 @@ if (!$page_name) {
 	<base target="_top">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	<link href="/img/favicon.ico" rel=icon>
+	
+	<title>FindMyProtest</title>
 	
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/common.css">
@@ -47,6 +42,7 @@ if (!$page_name) {
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="/js/common.js"></script>
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
 </head>
 
 <body>
