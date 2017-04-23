a4fbaa3c46ba5bfe138704cf2e029cd5a8c675b7
Jonathan Liang <j.liang8888@gmail.com>
2017-04-23 19:16:39 -0400

    Edited user profile
    Updated liked events section

--- user.php
+++ user.php
@@ -9,16 +9,22 @@ $likesArray = $db->query("SELECT * FROM events_saved LEFT JOIN events ON (events
 	<div class="col-md-12">
 		<h1 class="page-header text-center"><?=$userpage_user->full ?></h1>
 	</div>
-	
+
 	<div class="col-md-6 col-md-offset-3">
 		<div class="panel panel-default">
 			<div class="panel-body">
 				<h2>User: <?=$userpage_user->username ?></h2>
-				<h3>Liked events</h3>
+				<h3>Liked events: </h3>
 
 				<table class="table"><tbody>
 					<?php
-					foreach($likesArray as $like) {
+					if(!$likesArray){
+						<tr><td>
+							<p>No liked events yet</p>
+						</td></tr>
+					}
+					else
+						foreach($likesArray as $like) {
 					?>
 					<tr><td>
 						<?=$like->name?>
